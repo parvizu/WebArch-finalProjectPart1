278458b6db1c85a3025e922f6cd9e29b4fb2cc80
Ramit Malhotra <ramitmalhotra@gmail.com>
2013-12-04 13:12:01 -0800

    part 1 files in server

--- server/app.py
+++ server/app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+import string
+import random
 import shelve
 from subprocess import check_output
 import flask
-from flask import request
+from flask import request, abort
 from os import environ
 
 app = flask.Flask(__name__)
 app.debug = True
 
 db = shelve.open("shorten.db")
-
+shortUrlDict={}
 
 ###
 # Home Resource:
@@ -18,14 +20,69 @@ db = shelve.open("shorten.db")
 ###
 @app.route('/home', methods=['GET'])
 def home():
-    """Builds a template based on a GET request, with some default
-    arguements"""
-    index_title = request.args.get("title", "i253")
-    hello_name = request.args.get("name", "Jim")
+    """Builds a template based on a GET request"""
     return flask.render_template(
-            'home.html',
-            title=index_title,
-            name=hello_name)
+            'index.html')
+
+###
+# Home Resource:
+# Only supports the GET method, returns a homepage represented as HTML
+###
+@app.route('/about', methods=['GET'])
+def about():
+    """Builds a template based on a GET request"""
+    return flask.render_template(
+            'about.html')
+    
+###
+# Shorts Resource:
+# Only supports the POST method, returns the shortened URL for the entered long url
+###
+@app.route('/shorts', methods=['POST'])
+def shorts():
+    """Shortens the given url, and returns the association between the original and the shortened url"""
+    #short_url= request.form["shortUrl"]
+    long_url = request.form["longUrl"]
+    short_url=request.form.get("shortUrl","empty!")
+    urlDict = {v:k for k, v in db.items()}
+    if(long_url in urlDict and short_url=="empty!"):
+        short_url=urlDict[long_url]
+        return short_url+"-old"
+    elif(short_url != "empty!"):
+        if isinstance(short_url, unicode):
+            short_url = short_url.encode('utf-8')
+        if(short_url in db):
+            del db[short_url]
+        db[short_url]=long_url
+        return short_url+"-stored"
+    elif(short_url == "empty!"):
+        short_url="r.pj/"+ id_generator()
+        db[short_url]=long_url
+        return short_url+"-new"
+
+###
+# Random character generator:
+# Helper function to automatically spit out a short URL path"
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+###
+# Short Resource:
+# Only supports the GET method, returns the shortened URL for the entered long url
+###
+@app.route('/short/<path:url>', methods=['GET'])
+def short(url):
+    """Redirects to the original url after looking up the association if it exists. Otherwise throws error"""
+    if isinstance(url, unicode):
+            url = url.encode('utf-8')
+    try:
+        longUrl=db[url]
+    except KeyError:
+        longUrl="error"
+        
+    if(longUrl=="error"):
+        return flask.render_template('error.html')        
+    else:
+        return flask.redirect(longUrl)
 
 
 ###
@@ -81,3 +138,4 @@ def i253():
 
 if __name__ == "__main__":
     app.run(port=int(environ['FLASK_PORT']))
+    
